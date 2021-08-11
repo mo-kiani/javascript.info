2d0ffba7d409772e33a1be33a0d4a3c3b6ef046a
Mo Kiani <mo.kiani.engineer@gmail.com>
2021-08-11 12:24:56 -0700

    complete task 1.2.7.1

---
+++ Part 1/2.7 Basic operators, maths/Task 1/main.js
@@ -0,0 +1,10 @@
+let a = 1, b = 1;
+
+let c = ++a;  
+// a = 2 because it has been incremented once from original value of 1
+// c = 2 because the prefix increment operator returns the incremented value
+
+let d = b++; 
+// b = 2 because it has been incremented once from original value of 1
+// d = 1 because the postfix increment operator returns the original value
+
