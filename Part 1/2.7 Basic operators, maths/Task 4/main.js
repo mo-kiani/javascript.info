31b869bcdeb8eabcef5f5a01a4f256a31b996675
Mo Kiani <mo.kiani.engineer@gmail.com>
2021-08-11 12:51:11 -0700

    complete task 1.2.7.4

---
+++ Part 1/2.7 Basic operators, maths/Task 4/main.js
@@ -0,0 +1,8 @@
+let a = prompt("First number?", 1);
+let b = prompt("Second number?", 2);
+
+alert(+a + +b);  
+// We need to convert the strings returned by prompt to numbers before
+// performing the arithmetic so that the binary plus operator performs 
+// arithmetic rather than string concatentation.
+
