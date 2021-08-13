44565666c2c9fd18ea31e18062550a16801271ee
Mo Kiani <mo.kiani.engineer@gmail.com>
2021-08-12 22:54:08 -0700

    complete task 1.2.10.2

---
+++ Part 1/2.10 Conditional branching/Task 2/main.js
@@ -0,0 +1,11 @@
+// Task 1.2.10.2
+
+let response = prompt(`What's the "official" name of JavaScript?`);
+
+if ( response === "ECMAScript" ) {
+    alert("Right!");
+}
+else {
+    alert("You don't know? ECMAScript!");
+}
+
