4176faeac65741b38620c1902d8249ac5f397879
Mo Kiani <mo.kiani.engineer@gmail.com>
2021-09-05 02:56:43 -0700

    complete task 1.6.10.5

---
+++ 1 - The JavaScript language/06 - Advanced working with functions/10 - Function binding/Task 5/main.js
@@ -0,0 +1,18 @@
+// Task 1.6.10.5
+
+function askPassword(ok, fail) {
+  let password = prompt("Password?", '');
+  if (password == "rockstar") ok();
+  else fail();
+}
+
+let user = {
+  name: 'John',
+
+  login(result) {
+    alert( this.name + (result ? ' logged in' : ' failed to log in') );
+  }
+};
+
+askPassword( user.login.bind(user, true), user.login.bind(user, false) );
+
