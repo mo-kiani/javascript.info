98b0af840b32f1df7c191431912076671c4443b7
Mo Kiani <mo.kiani.engineer@gmail.com>
2021-09-17 23:51:46 -0700

    complete task 1.9.2.1

---
+++ 1 - The JavaScript language/09 - Classes/2 - Class inheritance/Task 1/main.js
@@ -0,0 +1,20 @@
+// Task 1.9.2.1
+
+class Animal {
+
+  constructor(name) {
+    this.name = name;
+  }
+
+}
+
+class Rabbit extends Animal {
+  constructor(name) {
+    super(name);
+    this.created = Date.now();
+  }
+}
+
+let rabbit = new Rabbit("White Rabbit"); 
+alert(rabbit.name);
+
